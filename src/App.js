83c4c1fe0bbc0d01c7780b25cb073f8ca7ca2e71
waleedamad <128586314+waleedamad@users.noreply.github.com>
2023-10-08 09:57:39 +0500

    hac

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import Header from './components/HeaderFolder/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Mainheader from './MainHeader/Mainheader';
+import Home from './Home/Home';
+import Contact from './Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Mainheader/>}>
+      <Route index element={<Home/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
